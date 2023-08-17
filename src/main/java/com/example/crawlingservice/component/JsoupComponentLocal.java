b7a789f152104900c20fe54f90ce655899a4cc32
bobo <qhfka8253@naver.com>
2023-08-17 15:42:47 +0900

    코드 정리 및 각 데이터 list에 임시 저장 후 DB에 저장

--- src/main/java/com/example/crawlingservice/component/JsoupComponentLocal.java
+++ src/main/java/com/example/crawlingservice/component/JsoupComponentLocal.java
@@ -17,47 +17,77 @@ import java.util.*;
 public class JsoupComponentLocal {
     private final FoodService foodService;
 
-    public void getInfo() {
+    public  void getInfo() {
         final String URL = "https://www.cbnucoop.com/service/restaurant/?week=0";
 
         try {
             Document doc = Jsoup.connect(URL).get();
-            String menu = getmenu(doc); //메인 메뉴 이름 정보 조회
-
-            System.out.println(menu);
+            Object menu = getmenu(doc); //메인 메뉴 이름 정보 조회
+            //System.out.println(menu);
 
         } catch (IOException ignored) {
         }
     }
 
-    public String getmenu(Document document) {
+    public  Object getmenu(Document document) {
+        Elements dateHtml = document.select(".weekday-title");
+        Elements menuHtml = document.select("#menu-result > div.menu");
+        Food foodObject = null;
 
-        Map<Integer, String> map = new LinkedHashMap<Integer, String>();//ID-대표메뉴 저장
+        ArrayList<String> dateList = new ArrayList<>();
+        ArrayList<String> idList = new ArrayList<>();
+        ArrayList<String> foodList = new ArrayList<>();
 
-        Elements menu = document.select("#menu-result > div.menu");
-        for (Element element : menu) {
-            Food foodObject = new Food();
 
-            Elements header = element.getElementsByClass("card-header");
-            String food = header.text();
+
+         for (Element element : dateHtml) {
+            dateList.add(element.text());
+        }
+
+        for (Element element : menuHtml) {
+            Elements headerHtml = element.getElementsByClass("card-header");
+            foodList.add(headerHtml.text());
 
             String table = element.attr("data-table");
             String word = table.replaceAll("-", "");//-제거
-            String id = word.substring(word.length() - 3, word.length());//끝에 3글자 추출
-
-            map.put(Integer.valueOf(id), food);
-
-            foodObject.setMenu(food);   //디비에 저장
-            division(id,foodObject);
-            System.out.println(foodObject);
+            idList.add(word.substring(word.length() - 3, word.length()));//끝에 3글자 추출
         }
-        return map.toString();
+
+        for(int i=0;i<foodList.size();i++){
+            foodObject = new Food();
+
+            foodObject.setMenu(foodList.get(i));   //디비에 저장
+            //foodObject.setDate(dateList.get(i));
+            division(idList.get(i),foodObject);
+
+            switch (idList.get(i).substring(idList.get(i).length()-1)){
+                case"0":
+                    foodObject.setDate(dateList.get(0));
+                    break;
+                case"1":
+                    foodObject.setDate(dateList.get(1));
+                    break;
+                case"2":
+                    foodObject.setDate(dateList.get(2));
+                    break;
+                case"3":
+                    foodObject.setDate(dateList.get(3));
+                    break;
+                case"4":
+                    foodObject.setDate(dateList.get(4));
+                    break;
+            }
+
+            foodService.save(foodObject);
+
+        }
+        return foodObject;
     }
 
 
-    public Object division (String parameter, Food food) {
+    public Object division(String parameter, Food food) {
 
-        switch (parameter.substring(0, 2)){
+        switch (parameter.substring(0,2)){
             case "25" :
                 food.setRest("은하수");
                 food.setTime("저녁");
@@ -83,27 +113,7 @@ public class JsoupComponentLocal {
                 food.setTime("석식");
                 break;
         }
-        switch (parameter.substring(parameter.length()-1)){
-            case"0":
-                food.setDate("월");
-                break;
-            case"1":
-                food.setDate("화");
-                break;
-            case"2":
-                food.setDate("수");
-                break;
-            case"3":
-                food.setDate("목");
-                break;
-            case"4":
-                food.setDate("금");
-                break;
-        }
-
-        foodService.save(food);
-
-        return food;
+        return null;
     }
 
 }
